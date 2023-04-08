8c83e83687d1332f04b4c3707943633939d5069d
erhanatalan <erhanatalan2013@gmail.com>
2023-04-08 18:12:56 +0300

    blog slice

---
+++ src/features/blogSlice.jsx
@@ -0,0 +1,38 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const blogSlice = createSlice({
+  name: "blog",
+
+  initialState: {
+    currentUser: null,
+    loading: false,
+    error: false,
+    isAdmin: false,
+    token: null,
+  },
+  reducers: {
+    fetchStart: (state) => {
+      state.loading = true;
+      state.error = false;
+    },
+    loginSuccess: (state, { payload }) => {
+      state.loading = false;
+      state.currentUser = payload?.user?.username;
+      state.isAdmin = payload?.user?.is_superuser;
+      state.token = payload?.key;
+    },
+    fetchFail: (state) => {
+      state.loading = false;
+      state.error = true;
+    },
+  },
+});
+
+export const {
+  fetchStart,
+  loginSuccess,
+  logoutSuccess,
+  registerSuccess,
+  fetchFail,
+} = blogSlice.actions;
+export default blogSlice.reducer;
